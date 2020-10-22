b404a5229589b507b2b49d6635d70cfe3b932ca9
Misael Gómez <62974284+Misael-GO@users.noreply.github.com>
2020-10-21 21:00:20 -0500

    Add files via upload

---
+++ genera.py
@@ -0,0 +1,19 @@
+"""
+En el comentario del archivo anterior, hice varias cosas, 
+para poder sacar la matriz de probabilidades, 
+ahora hay que hacer un programa que a partir de 10 tweets genere la misma 
+matriz de probabilidades
+sabiendo de ante mano cuales son los de stream
+
+Sugiero esta estructura, para lo tweets
+
+{
+	"Tweets" : [
+		{"Stream":true, "texto":"Vamos a darle a Among Us con famosos"},
+		{"Stream":false, "texto":"El Fugitivo: La Historia en 1 Video"},
+	]
+}
+
+y debe generar el  json compatible con el programa de clasificacion
+
+"""
